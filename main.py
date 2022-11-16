94b49bd633a425aa5500acd770f30e3016291cf8
BradySC <bscottcarpenter19@gmail.com>
2022-11-16 17:01:39 -0600

    added ability to handle radius around fixed point and work around the FAAs change to formatting of 5G airspace notams

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, re
+import os, re, math
 import pandas as pd
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
@@ -52,10 +52,23 @@ class NOTAM:
         text_lines = self.NotamText.splitlines()
         for index, line in enumerate(text_lines):
             if 'RADIUS OF ' in line:
-                center_point = re.findall('([0-9]{6}(N|S)[0-9]{7}(E|W))', line)[0]
+                radius = float(line[:4]) if 'NM' not in line[:4] else float(line[:2])
+                center_point = re.findall('([0-9]{6}(N|S)[0-9]{7}(E|W))', line)[0][0]
+                centerLat, centerLon = parse_coords(center_point)
+                sample_points = 44
+                circle_coords = []
+                for k in range(1,sample_points):
+                    angle = math.pi*2*k/sample_points
+                    dx = radius*math.cos(angle)
+                    dy = radius*math.sin(angle)
+                    lat = centerLat + (180/math.pi) * (dy/3443.92)
+                    lon = centerLon + (180/math.pi) * (dx/3443.92)/math.cos(centerLat*math.pi/180)
+                    circle_coords.append((lat,lon))
             else:
+                radius = None
                 center_point = None
-        return center_point
+                circle_coords = None
+        return circle_coords
 
     def __str__(self):
         return (
@@ -83,6 +96,9 @@ def read_xlsx(filename):
     notams_df.rename(columns={'NOTAM Condition/LTA subject/Construction graphic title': 'NOTAM Text'}, inplace=True)
     notams_df = notams_df.loc[notams_df['Class'] == 'Airspace'].copy()
     notams_dict = notams_df.to_dict('records')
+    for notam in notams_dict:
+        notam['NOTAM Text'] = notam['NOTAM Text'].replace('UNREL WI AN AREA DEFINED AS ', 'UNREL WI AN AREA DEFINED AS \n')
+        notam['NOTAM Text'] = notam['NOTAM Text'].replace(') TO ', ') TO\n')
     return notams_dict
 
 def parse_coords(coord_string):
@@ -98,35 +114,47 @@ def parse_coords(coord_string):
     lng = (int(lng_unparse[:3]) + (int(lng_unparse[3:5])/60) + (int(lng_unparse[5:])/3600)) * signs[lng_dir]
     return lat,lng
 
-def build_kml(kml_string, Notam):
+def build_kml(kml_string, Notam, circle):
     notam_name = '%s - %s' % (Notam.Location, Notam.Identification)
-    kml_string = kml_string.replace('<Document><name>My document</name>',('<Document><name>%s</name>' % notam_name))
     kml_string = kml_string.replace('<Placemark><name>NAME</name>',('<Placemark><name>%s</name>' % notam_name))
     kml_string = kml_string.replace('<description>YES</description>',('<description>%s</description>' % Notam))
-    for index, coord in enumerate(Notam.CoordsStrList):
-        lat, lng = parse_coords(coord)
-        kml_string += ('\n%f,%f,0.0' % (lng, lat))
+    if circle:
+        for coord in Notam.CircleCoords:
+            lat, lng = coord[0], coord[1]
+            kml_string += ('\n%f,%f,1.0' % (lng, lat))
 
-    lat, lng = parse_coords(Notam.CoordsStrList[0])
-    kml_string += ('\n%f,%f,0.0\n' % (lng, lat))
+        lat, lng = Notam.CircleCoords[0]
+        kml_string += ('\n%f,%f,1.0\n' % (lng, lat))
+    else:
+        for index, coord in enumerate(Notam.CoordsStrList):
+            lat, lng = parse_coords(coord)
+            kml_string += ('\n%f,%f,1.0' % (lng, lat))
+
+        lat, lng = parse_coords(Notam.CoordsStrList[0])
+        kml_string += ('\n%f,%f,1.0\n' % (lng, lat))
     return kml_string
 
 def main(filename):
     notams_dict = read_xlsx(filename)
     kml_start = open(os.path.join(dir_path, 'kml_front_matter.kml'), 'r').read()
     kml_middle = open(os.path.join(dir_path, 'kml_int_matter.kml'), 'r').read()
+    kml_bbox_start = open(os.path.join(dir_path, 'kml_bbox_start_matter.kml'), 'r').read()
+    kml_bbox_end = open(os.path.join(dir_path, 'kml_bbox_end_matter.kml'), 'r').read()
+    kml_circ_start = open(os.path.join(dir_path, 'kml_circ_start_matter.kml'), 'r').read()
+    kml_circ_end = open(os.path.join(dir_path, 'kml_circ_end_matter.kml'), 'r').read()
     kml_end = open(os.path.join(dir_path, 'kml_end_matter.kml'), 'r').read()
     circle_check = 0
     for index, notam in enumerate(notams_dict):
         Notam = NOTAM(notam)
-        if Notam.circle:
-            if index == 0:
-                circle_check = index+1
-            continue
-        if circle_check == index and index == 1:
-            kml = build_kml(kml_start, Notam)
-        else:
-            kml += build_kml(kml_middle, Notam)
+        if index == 0:
+            kml = build_kml(kml_start, Notam, False)
+            kml += kml_bbox_end
+        elif index != 0 and Notam.circle:
+            kml += build_kml(kml_circ_start, Notam, True)
+            kml += kml_circ_end
+        elif index != 0 and not Notam.circle:
+            kml += build_kml(kml_bbox_start, Notam, False)
+            kml += kml_bbox_end
     
     kml += kml_end
     with open(os.path.join(dir_path, 'kml_output.kml'), 'w+') as f:
